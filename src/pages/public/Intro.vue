29c9d5aad6d58c43e425478549c9f289c8cf797a
Paulo Henrique Motta <motta.phenrique@gmail.com>
2024-08-25 23:15:49 -0300

    atualização pagina de events info

--- src/pages/public/Intro.vue
+++ src/pages/public/Intro.vue
@@ -1,131 +1,163 @@
 <template>
   <div>
-    <v-container fluid>
-      <v-container fluid class="d-flex flex-column pa-0">
-        <div class="mb-8 text-center">
-          <h1 class="display-1">
-            Só os
-            <strong style="color: #f2af29">melhores </strong>
-            <strong style="color: #38be92">eventos</strong>
-            para você na nossa plataforma
-          </h1>
-        </div>
+    <v-container
+      class="d-flex justify-center"
+      style="background-color: #38be9233; max-width: 2000px"
+    >
+      <v-row>
+        <v-col
+          cols="12"
+          class="d-flex justify-center flex-column text-center align-center my-"
+          style="flex-basis: inherit"
+        >
+          <p><strong>Principais recursos</strong></p>
 
-        <SearchInput />
+          <h1 class="my-3">Tudo o que você precisa para organizar eventos.</h1>
 
-        <HighlightEvents class="my-3" />
-
-        <div class="d-flex justify-center my-2 align-center">
-          <v-btn
-            style="
-              background-color: #38be92;
-              color: #fff;
-              font-weight: bold;
-              width: 200px;
-            "
-          >
-            Ver mais Eventos
-            <v-icon size="large">mdi-chevron-right</v-icon>
-          </v-btn>
-        </div>
-      </v-container>
-
-      <v-container fluid class="py-10" style="background-color: #38be9233">
-        <v-row class="d-flex justify-between" align="center" no-gutters>
-          <v-col
-            cols="12"
-            md="6"
-            class="d-flex justify-center flex-column align-center text-center"
-          >
-            <h1>Eventos irados para você</h1>
-            <p>
-              Encontre o evento ideal para participar, só os melhores
-              palestrantes você encontra aqui!
-            </p>
-          </v-col>
-
-          <v-col
-            cols="12"
-            md="6"
-            class="d-flex justify-center flex-column align-center mt-6 mt-md-0"
-          >
-            <v-btn
-              style="background-color: #fff; color: #38be92; font-weight: bold"
+          <p>
+            <strong>
+              Nosso sistema de gerenciamento de eventos inclui recursos
+              avançados de credenciamento, venda de ingressos, relatórios e
+              muito mais.</strong
             >
-              Ver mais Eventos <v-icon size="large">mdi-chevron-right</v-icon>
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-container>
+          </p>
+        </v-col>
+
+        <v-container class="justify-between pa-0">
+          <v-row class="justify-center">
+            <v-col
+              xs="12"
+              sm="12"
+              md="12"
+              lg="6"
+              class="d-flex justify-center mt-5 flex-column text-xs-center text-md-center text-sm-center text-lg-start"
+              style="flex-basis: inherit"
+            >
+              <h3 class="mt-lg-10">Credenciamento Facial</h3>
+              <p class="text-recurse">
+                Credenciamento rápido e seguro com reconhecimento facial.
+              </p>
+
+              <h3>Venda de Ingressos</h3>
+              <p class="text-recurse">
+                Venda de ingressos online com opções de pagamento.
+              </p>
+
+              <h3>Relatórios avançados</h3>
+              <p class="text-center">
+                Acompanhe todos os dados do seu evento com nossos relatórios.
+              </p>
+            </v-col>
+
+            <v-col
+              xs="12"
+              sm="12"
+              md="12"
+              lg="6"
+              class="d-flex justify-center pb-5"
+              style="flex-basis: inherit"
+            >
+              <v-img
+                src="../../assets/event-info/dashboard-image.png"
+                class="centered-img"
+                rounded
+              ></v-img>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-row>
+    </v-container>
+
+    <v-container
+      class="d-flex justify-center p-10 align-center"
+      style="background-color: #38be92; max-width: 2000px"
+    >
+      <v-row class="w-50">
+        <v-col
+          xs="12"
+          sm="12"
+          md="12"
+          lg="6"
+          class="d-flex justify-center flex-column p-10"
+        >
+          <h1 class="text-center text-lg-start text-white">
+            Crie Seus eventos com facilidade
+          </h1>
+
+          <p>
+            Nossos sistema de credenciamento facial e venda de ingressos online
+            torna a organização de eventos muito mais simples e eficiente
+          </p>
+        </v-col>
+        <v-col
+          xs="12"
+          md="12"
+          lg="6"
+          class="d-flex justify-center flex-column align-center"
+        >
+          <v-btn
+            style="background-color: #fff; color: #38be92; font-weight: bold"
+          >
+            EXPERIMENTE AGORA
+          </v-btn>
+        </v-col>
+
+        <v-responsive width="100%"></v-responsive>
+      </v-row>
     </v-container>
   </div>
 </template>
 
 <script setup>
-
-import { useRoute } from 'vue-router';
-const route = useRoute()
-
+import { useRoute } from "vue-router";
+const route = useRoute();
 </script>
 
 <style scoped>
-h1 {
-  width: 70vh;
-  font-size: 3.1rem;
-  line-height: 1.2;
-  text-transform: uppercase;
-  font-weight: 500;
-  word-spacing: 5px;
-  text-align: start;
+.text-recurse {
+  margin-bottom: 5%;
+  text-align: center;
 }
 
-@media (max-width: 768px) {
+h3 {
+  color: #38be92;
+  text-transform: uppercase;
+  font-weight: 500;
+  text-align: center;
+}
+
+h1 {
+  color: #38be92;
+}
+
+p {
+  opacity: 70%;
+}
+
+.centered-img {
+  width: 80%;
+  height: 80%;
+  margin-top: 70px;
+}
+
+.p-10 {
+  padding: 40px;
+}
+
+@media (min-width: 600px) {
+  .v-container {
+    padding: 24px; /* Padding for tablets */
+  }
+
   h1 {
-    width: 100%; /* Ajuste a largura para caber no mobile */
-    font-size: 2rem; /* Diminui o tamanho da fonte */
-    text-align: center; /* Centraliza o texto */
-    word-spacing: normal; /* Ajuste de espaçamento */
-  }
-
-  .v-btn {
-    width: 100%;
-  }
-
-  .mb-8 {
-    margin-bottom: 16px !important;
-  }
-
-  .my-3 {
-    margin-top: 16px !important;
-    margin-bottom: 16px !important;
-  }
-
-  .my-2 {
-    margin-top: 8px !important;
-    margin-bottom: 8px !important;
-  }
-
-  .v-col {
-    margin-top: 16px !important;
-  }
-
-  h1 {
-    font-size: 1.8rem;
+    font-size: 2rem; /* Font size for tablets */
+    text-align: left;
   }
 
   p {
-    font-size: 1rem;
+    font-size: 1rem; /* Font size for tablets */
+    text-align: left;
   }
-}
-
-.img-bg {
-  position: absolute;
-  top: 0;
-}
-
-.container-absolute {
-  position: relative;
-  top: 0;
 }
 </style>
 
